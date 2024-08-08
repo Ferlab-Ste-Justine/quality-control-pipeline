6b3ababf9da27f66c5d691dbc7bbcaa3b41babf4
Remrem08007 <Sachou08@yahoo.com>
2024-08-08 13:08:49 -0400

    feat : Added the required modules to do QC on bam files

---
+++ bin/analyse_mosdepth.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import pandas as pd
+
+couverture_region_minimale = float(sys.argv[1])
+couverture_mediane_minimale = float(sys.argv[2])
+nombre_std = float(sys.argv[3])
+couverture_minimale_normale = float(sys.argv[4])
+path_coverage_per_contig = sys.argv[5]
+path_multiqc_general_stats = sys.argv[6]
+
+
+mosdepth_per_region_df = pd.read_table(
+    path_coverage_per_contig,
+    usecols=['Sample', 'chr1', 'chr2',
+             'chr3', 'chr4', 'chr5', 'chr6',
+             'chr7', 'chr8', 'chr9',
+             'chr10', 'chr11', 'chr12',
+             'chr13', 'chr14', 'chr15',
+             'chr16', 'chr17', 'chr18',
+             'chr19', 'chr20', 'chr21',
+             'chr22', 'chrX', 'chrY'],)
+mosdepth_per_region_df.fillna(0, inplace=True)
+
+problems = []
+
+if mosdepth_per_region_df.isin([0]).any().any():
+    print("ERREUR : probleme a cause de la courverture par region pour les" +
+          " echantillons:")
+
+    for index, value in mosdepth_per_region_df['Sample'].items():
+        if 0 in mosdepth_per_region_df.loc[index].values:
+            print(f"{mosdepth_per_region_df.loc[index, 'Sample']}")
+            for column in mosdepth_per_region_df.columns:
+                if column == 'Sample':
+                    continue
+                try:
+                    cell_value = mosdepth_per_region_df.loc[index, column]
+                    if cell_value is None or cell_value == "None" or cell_value == '':
+                        cell_value = 0.0
+                    if float(cell_value) <= couverture_region_minimale:
+                        problems.append([mosdepth_per_region_df.loc[index,
+                                                                    'Sample'],
+                                         column, cell_value])
+                except (ValueError, TypeError) as e:
+                    problems.append([mosdepth_per_region_df.loc[
+                        index, 'Sample'], column,
+                                     f"Erreur: {e}"])
+
+    problems_df = pd.DataFrame(problems, columns=['Sample', 'Chromosome',
+                                                  'Coverage'])
+
+    # Sort by Sample and Chromosome, handling chrX and chrY separately
+    problems_df['Chromosome'] = problems_df['Chromosome'].apply(
+        lambda x: (23 if x == 'chrX' else (24 if x == 'chrY' 
+                                           else int(x.replace('chr', '')))))
+    problems_df.sort_values(by=['Sample', 'Chromosome'], inplace=True)
+    problems_df['Chromosome'] = problems_df['Chromosome'].apply(
+        lambda x: ('chrX' if x == 23 else ('chrY' if x == 24 else f'chr{x}')))
+
+    print("Pour plus de détails sur les régions manquantes," +
+          " veuillez consulter le fichier de sortie " +
+          "\'erreurs_de_couvertures_par_region.txt\'\n")
+    
+    if not os.path.exists('errors'):
+        os.makedirs('errors')
+
+    try:
+        # Ouvrir le fichier en mode écriture
+        with open('errors/erreurs_de_couvertures_par_region.txt', 'w') as f:
+            f.write("Sample\tChromosome\tCoverage\n")
+            for _, row in problems_df.iterrows():
+                f.write(f"{row['Sample']}\t{row['Chromosome']}\t" +
+                        f"{row['Coverage']}\n")
+    except IOError as e:
+        print(f"Erreur lors de l'ouverture ou de l'écriture du fichier: {e}")
+    except Exception as e:
+        print(f"Une erreur inattendue est survenue: {e}")
+else:
+    print("Toutes les régions ont ete correctement couvertes.")
+
+mosdepth_general_df = pd.read_table(path_multiqc_general_stats,
+                                    usecols=['Sample',
+                                             'Mosdepth_mqc-generalstats-' +
+                                             'mosdepth-median_coverage',
+                                             'Mosdepth_mqc-generalstats-' +
+                                             'mosdepth-1_x_pc', 'Mosdepth_mqc'
+                                             + '-generalstats-mosdepth-5_x_pc',
+                                             'Mosdepth_mqc-generalstats-' +
+                                             'mosdepth-10_x_pc',
+                                             'Mosdepth_mqc-generalstats-' +
+                                             'mosdepth-30_x_pc',
+                                             'Mosdepth_mqc-generalstats-' +
+                                             'mosdepth-50_x_pc'],)
+mosdepth_general_df.fillna(0, inplace=True)
+
+if mosdepth_general_df['Mosdepth_mqc-generalstats-' +
+                       'mosdepth-median_coverage'].isin([0]).any():
+    print("ERREUR : probleme a cause de la courverture médianne pour les" +
+          " echantillons:")
+
+    problems = []
+
+    for index, row in mosdepth_general_df.iterrows():
+        try:
+            cell_value = row['Mosdepth_mqc-generalstats-mosdepth-' +
+                             'median_coverage']
+            if cell_value is None or cell_value == "None" or cell_value == '':
+                cell_value = 0.0
+            if float(cell_value) <= couverture_mediane_minimale:
+                sample_name = row['Sample']
+                print(f"Sample: {sample_name}")
+                problems.append([sample_name, cell_value])
+        except (ValueError, TypeError) as e:
+            problems.append([row['Sample'], f"Erreur: {e}"])
+
+    # Create a DataFrame for problems
+    problems_df = pd.DataFrame(problems, columns=['Sample', 'Median_coverage'])
+
+    print("Pour plus de détails sur les régions manquantes, veuillez" +
+          " consulter le fichier de sortie " +
+          "'erreurs_de_couvertures_mediane.txt'")
+
+    try:
+        # Write the details to the output file
+        with open('errors/erreurs_de_couvertures_mediane.txt', 'w') as f:
+            f.write("Sample\tMedian_coverage\n")
+            for _, row in problems_df.iterrows():
+                f.write(f"{row['Sample']}\t{row['Median_coverage']}\n")
+    except IOError as e:
+        print(f"Erreur lors de l'ouverture ou de l'écriture du fichier: {e}")
+    except Exception as e:
+        print(f"Une erreur inattendue est survenue: {e}")
+else:
+    print("Tous les echantillons ont une couverture medianne appropriee.")
+
+
+# Fonction pour détecter les valeurs anormales
+def detect_outliers(df, column, n_std=nombre_std):
+    mean = df[column].mean()
+    std = df[column].std()
+    lower_bound = mean - n_std * std
+    return df.loc[(df[column] < lower_bound) & (
+        df[column] < couverture_minimale_normale)]
+
+
+# Détecter les valeurs anormales pour chaque colonne de couverture
+outliers = pd.DataFrame()
+for column in [
+    'Mosdepth_mqc-generalstats-mosdepth-1_x_pc',
+    'Mosdepth_mqc-generalstats-mosdepth-5_x_pc',
+    'Mosdepth_mqc-generalstats-mosdepth-10_x_pc',
+    'Mosdepth_mqc-generalstats-mosdepth-30_x_pc',
+    'Mosdepth_mqc-generalstats-mosdepth-50_x_pc'
+]:
+    column_outliers = detect_outliers(mosdepth_general_df, column)
+    outliers = pd.concat([outliers, column_outliers[['Sample', column]]])
+
+if not (outliers['Mosdepth_mqc-generalstats-mosdepth-1_x_pc'].isna().all() and outliers['Mosdepth_mqc-generalstats-mosdepth-5_x_pc'].isna().all()):
+    print("Pour plus de détails sur les échantillons avec des valeurs de " +
+          "couverture anormales, veuillez consulter le fichier de sortie " +
+          "'erreurs_de_couvertures_anormales.txt'")
+    try:
+        # Écrire les détails dans le fichier de sortie
+        with open('errors/erreurs_de_couvertures_anormales.txt', 'w') as f:
+            f.write("Sample\t1x\t5x\t10x\t30x\t50x\n")
+            for _, row in outliers.iterrows():
+                f.write(f"{row['Sample']}\t{row['Mosdepth_mqc-generalstats-mosdepth-1_x_pc']}\t{row['Mosdepth_mqc-generalstats-mosdepth-5_x_pc']}\t{row['Mosdepth_mqc-generalstats-mosdepth-10_x_pc']}\t{row['Mosdepth_mqc-generalstats-mosdepth-30_x_pc']}\t{row['Mosdepth_mqc-generalstats-mosdepth-50_x_pc']}\n")
+    except IOError as e:
+        print(f"Erreur lors de l'ouverture ou de l'écriture du fichier: {e}")
+    except Exception as e:
+        print(f"Une erreur inattendue est survenue: {e}")
+else:
+    print("Toutes les echantillons ont des couvertures normales.")
---
+++ bin/analyse_samtools.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+
+import os
+import statistics
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+path_multiqc_samtools_stats = sys.argv[1]
+
+reads_mapped = []
+sample_names = []
+reads_MQ0 = []
+reads_unmapped = []
+reads_QC_failed_percent = []
+flagged_low_reads_samples = []
+flagged_high_MQ0_samples = []
+flagged_high_unmapped_samples = []
+flagged_high_reads_QC_failed_percent = []
+
+samtools_df = pd.read_table(path_multiqc_samtools_stats, 
+                            usecols=['reads_mapped', 'Sample', 'reads_MQ0',
+                                     'reads_unmapped',
+                                     'reads_QC_failed_percent'],)
+samtools_df.fillna(0, inplace=True)
+samtools_df.sort_values(by=['reads_mapped'], inplace=True)
+
+reads_mapped = samtools_df.loc[:, 'reads_mapped'].tolist()
+sample_names = samtools_df.loc[:, 'Sample'].tolist()
+reads_unmapped = samtools_df.loc[:, 'reads_unmapped'].tolist()
+reads_MQ0 = samtools_df.loc[:, 'reads_MQ0'].tolist()
+reads_QC_failed_percent = samtools_df.loc[:,
+                                          'reads_QC_failed_percent'].tolist()
+
+# print(reads_unmapped)
+# print(sample_names)
+
+average_reads_mapped = 0
+average_reads_MQ0 = 0
+average_reads_unmapped = 0
+average_reads_QC_failed_percent = 0
+
+if reads_mapped and reads_MQ0 and reads_unmapped and reads_QC_failed_percent:
+    average_reads_mapped = sum(reads_mapped) / len(reads_mapped)
+    average_reads_MQ0 = sum(reads_MQ0) / len(reads_MQ0)
+    average_reads_unmapped = sum(reads_unmapped) / len(reads_unmapped)
+    average_reads_QC_failed_percent = sum(reads_QC_failed_percent) / len(
+          reads_QC_failed_percent)
+    print("The average amount of mapped reads per sample is: " +
+          f"{average_reads_mapped}")
+    print("The average amount of reads with MQ of 0 per sample is: " +
+          f"{average_reads_MQ0}")
+    print("The average amount of unmapped reads per sample is: " +
+          f"{average_reads_unmapped}")
+    print("The average percentage of QC failed reads per sample is: " +
+          f"{average_reads_QC_failed_percent}\n")
+else:
+    print("Missing some valid values to calculate the averages.\n")
+
+min_reads_mapped = float("inf")
+min_reads_mapped_index = 0
+max_reads_unmapped = 0
+max_reads_unmapped_index = 0
+max_reads_MQ0 = 0
+max_reads_MQ0_index = 0
+max_reads_QC_failed_percent = 0
+max_reads_QC_failed_percent_index = 0
+stdev_reads_mapped = statistics.pstdev(reads_mapped)
+stdev_reads_MQ0 = statistics.pstdev(reads_MQ0)
+stdev_reads_unmapped = statistics.pstdev(reads_unmapped)
+stdev_reads_QC_failed_percent = statistics.pstdev(reads_QC_failed_percent)
+
+for i, read in enumerate(reads_mapped):
+    if read < (average_reads_mapped - 2 * stdev_reads_mapped):
+        print(f"The number of mapped reads for the sample {sample_names[i]}" +
+              " is unusually low")
+        flagged_low_reads_samples.append(i)
+    if read < min_reads_mapped:
+        min_reads_mapped = read
+        min_reads_mapped_index = i
+
+for i, mq0 in enumerate(reads_MQ0):
+    if mq0 > (average_reads_MQ0 + 3 * stdev_reads_MQ0):
+        print(f"The number of MQ0 reads for the sample {sample_names[i]}" +
+              " is unusually high")
+        flagged_high_MQ0_samples.append(i)
+    if mq0 > max_reads_MQ0:
+        max_reads_MQ0 = mq0
+        max_reads_MQ0_index = i
+
+for i, unmapped in enumerate(reads_unmapped):
+    if unmapped > (average_reads_unmapped + (3 * stdev_reads_unmapped)):
+        print(f"The number of unmapped reads for the sample {sample_names[i]}"
+              + " is unusually high")
+        flagged_high_unmapped_samples.append(i)
+    if unmapped > max_reads_unmapped:
+        max_reads_unmapped = unmapped
+        max_reads_unmapped_index = i
+
+for i, QC_failed_percent in enumerate(reads_QC_failed_percent):
+    if QC_failed_percent > (average_reads_QC_failed_percent +
+                            (3 * stdev_reads_QC_failed_percent)):
+        print(f"The number of unmapped reads for the sample {sample_names[i]}"
+              + " is unusually high")
+        flagged_high_reads_QC_failed_percent.append(i)
+    if QC_failed_percent > max_reads_QC_failed_percent:
+        max_reads_QC_failed_percent = QC_failed_percent
+        max_reads_QC_failed_percent_index = i
+
+print("\nThe minimum amount of mapped reads for a sample is: " +
+      str(min_reads_mapped) + " (" +
+      str(sample_names[min_reads_mapped_index]) + ")")
+print("The maximum amount of MQ0 reads for a sample is: " +
+      str(max_reads_MQ0) + " (" +
+      str(sample_names[max_reads_MQ0_index]) + ")")
+print("The maximum amount of unmapped reads for a sample is: " +
+      str(max_reads_unmapped) + " (" +
+      str(sample_names[max_reads_unmapped_index]) + ")")
+print("The maximum percentage of QC failed reads for a sample sample is: " +
+      str(max_reads_QC_failed_percent) + " (" +
+      str(sample_names[max_reads_QC_failed_percent_index]) + ")")
+
+# Plotting mapping data
+mapped = np.array(reads_mapped)
+mq0 = np.array(reads_MQ0)
+
+plt.barh(sample_names, reads_mapped, label='Mapped reads')
+plt.barh(sample_names, reads_MQ0,
+         left=reads_mapped, color='yellow', label='MQ0 reads')
+plt.barh(sample_names, reads_unmapped, left=mapped+mq0, color='red',
+         label='Unmapped reads')
+
+min_reads_mapped_index = np.argmin(reads_mapped)
+max_reads_mapped_index = np.argmax(reads_mapped)
+
+min_reads_mapped_value = reads_mapped[min_reads_mapped_index]
+max_reads_mapped_value = reads_mapped[max_reads_mapped_index]
+
+plt.axvline(min_reads_mapped_value, color='black', linestyle='--',
+            label=f'Min mapped reads: {min_reads_mapped_value} ' +
+            f'({sample_names[min_reads_mapped_index]})')
+plt.axvline(max_reads_mapped_value, color='green', linestyle='--',
+            label=f'Max mapped reads: {max_reads_mapped_value} ' +
+            f'({sample_names[max_reads_mapped_index]})')
+
+max_unmapped_index = np.argmax(reads_unmapped)
+highlight_sample = sample_names[max_unmapped_index]
+highlight_band_top = max_unmapped_index + 0.5
+highlight_band_bottom = max_unmapped_index - 0.5
+plt.axhspan(highlight_band_bottom, highlight_band_top, color='red', alpha=0.3,
+            label=f'Most unmapped reads: {highlight_sample}')
+
+plt.title('Représentation graphique du nombre de mapped reads, du nombre\n' +
+          'reads avec un mapping quality de 0 et du nombre de unmapped reads')
+plt.legend()
+
+if not os.path.exists('samtools_reads_plot'):
+    os.makedirs('samtools_reads_plot')
+
+plt.savefig('samtools_reads_plot/samtools_reads_info.png')
+
+# Plotting
